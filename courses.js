115b1f55cdbcbd50ea0577197f80918c6ee0e14a
Immanuel <immanuel.peter1@gmail.com>
2023-06-03 16:32:20 -0500

    Started to add Codecademy courses completed

---
+++ courses.js
@@ -0,0 +1,32 @@
+export const courses = {
+    codecademy: {
+        swift: {
+            course: "Learn Swift",
+            finishDate: "05-28-2022"
+        },
+        html: {
+            course: "Learn HTML",
+            finishDate: "06-12-2022"
+        },
+        css: {
+            course: "Learn CSS",
+            finishDate: "06-18-2022" 
+        },
+        cplusplus: {
+            course: "Learn C++",
+            finishDate: "07-01-2022" 
+        },
+        intermcplusplus: {
+            course: "C++ for Programmers",
+            finishDate: "07-05-2022"
+        },
+        java: {
+            course: "Learn Java",
+            finishDate: "07-13-2022"
+        },
+        java: {
+            course: "Learn Java",
+            finishDate: "07-13-2022"
+        },
+    }
+}
