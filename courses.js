c746302c8e24aa59ebfa9a0e2d681d455af5534b
Immanuel Peter <immanuel.peter1@gmail.com>
2023-06-04 00:44:02 -0500

    Finished Codecademy courses

--- courses.js
+++ courses.js
@@ -1,32 +1,134 @@
-export const courses = {
-    codecademy: {
-        swift: {
-            course: "Learn Swift",
-            finishDate: "05-28-2022"
-        },
-        html: {
-            course: "Learn HTML",
-            finishDate: "06-12-2022"
-        },
-        css: {
-            course: "Learn CSS",
-            finishDate: "06-18-2022" 
-        },
-        cplusplus: {
-            course: "Learn C++",
-            finishDate: "07-01-2022" 
-        },
-        intermcplusplus: {
-            course: "C++ for Programmers",
-            finishDate: "07-05-2022"
-        },
-        java: {
-            course: "Learn Java",
-            finishDate: "07-13-2022"
-        },
-        java: {
-            course: "Learn Java",
-            finishDate: "07-13-2022"
-        },
+export const codecademy = {
+    swift: {
+        course: "Learn Swift",
+        finishDate: "05-28-2022",
+        hours: 25,
+        weeks: null
+    },
+    html: {
+        course: "Learn HTML",
+        finishDate: "06-12-2022",
+        hours: 9,
+        weeks: null
+    },
+    css: {
+        course: "Learn CSS",
+        finishDate: "06-18-2022",
+        hours: 10,
+        weeks: null
+    },
+    cplusplus: {
+        course: "Learn C++",
+        finishDate: "07-01-2022",
+        hours: 25,
+        weeks: null
+    },
+    intermcplusplus: {
+        course: "C++ for Programmers",
+        finishDate: "07-05-2022",
+        hours: 9,
+        weeks: null
+    },
+    java: {
+        course: "Learn Java",
+        finishDate: "07-13-2022",
+        hours: 25,
+        weeks: null
+    },
+    beautifulsoup: {
+        course: "Learn Web Scraping with BeautifulSoup",
+        finishDate: "07-15-2022",
+        hours: 1,
+        weeks: null
+    },
+    intermswift: {
+        course: "Learn Intermediate Swift",
+        finishDate: "07-17-2022",
+        hours: 9,
+        weeks: null
+    },
+    cmdline: {
+        course: "Learn Command Line",
+        finishDate: "07-23-2022",
+        hours: 8,
+        weeks: null
+    },
+    javascript: {
+        course: "Learn JavaScript",
+        finishDate: "08-14-2022",
+        hours: 20,
+        weeks: null
+    },
+    kotlin: {
+        course: "Learn Kotlin",
+        finishDate: "10-23-2022",
+        hours: 15,
+        weeks: null
+    },
+    nflstats: {
+        course: "Analyze NFL Stats with Python Case Study",
+        finishDate: "10-26-2022",
+        hours: 1,
+        weeks: null
+    },
+    interactivejs: {
+        course: "Building Interactive JavaScript Websites",
+        finishDate: "11-05-2022",
+        hours: 7,
+        weeks: null
+    },
+    ioslevelone: {
+        course: "Build iOS Apps with SwiftUI",
+        finishDate: "12-28-2022",
+        hours: null,
+        weeks: 10
+    },
+    iosleveltwo: {
+        course: "Build an Intermediate iOS App with Swift UI",
+        finishDate: "01-14-2023",
+        hours: null,
+        weeks: 14
+    },
+    asyncjs: {
+        course: "Learn JavaScript: Asynchronous Programming",
+        finishDate: "01-14-2023",
+        hours: 1,
+        weeks: null
+    },
+    linalg: {
+        course: "Linear Algebra",
+        finishDate: "04-03-2023",
+        hours: 2,
+        weeks: null
+    },
+    chatgpt: {
+        course: "Intro to ChatGPT",
+        finishDate: "04-19-2023",
+        hours: 7,
+        weeks: null
+    },
+    jquery: {
+        course: "Learn jQuery",
+        finishDate: "05-05-2023",
+        hours: 9,
+        weeks: null
+    },
+    nodejs: {
+        course: "Learn Node.js",
+        finishDate: "05-14-2023",
+        hours: 7,
+        weeks: null
+    },
+    react: {
+        course: "Learn React",
+        finishDate: "05-30-2023",
+        hours: 15,
+        weeks: null
+    },
+    reactnative: {
+        course: "Learn React Native",
+        finishDate: "06-02-2023",
+        hours: 7,
+        weeks: null
     }
 }
