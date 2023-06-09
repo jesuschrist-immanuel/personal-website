48c2b81f86ae80a13702ba64ed06ffe3f07fb9cc
Immanuel Peter <immanuel.peter1@gmail.com>
2023-06-09 01:56:51 -0500

    Changed color of send button and message handler

--- responses.js
+++ responses.js
@@ -21,3 +21,13 @@ function getBotResponse(input) {
     }
     return 'woof';
 }
+
+async function getCompletionFromMessages( messages, model = 'gpt-3.5-turbo', temperature = 0 ) {
+    const completion = await openai.createChatCompletion({
+        model: model,
+        messages: messages,
+        temperature: temperature       // The degree of randomness in the output
+    });
+
+    return completion.data.choices[0].message.content;
+};
