238ee7442a96e304a7a3656482de33948a571609
Immanuel <immanuel.peter1@gmail.com>
2023-06-12 00:39:33 -0500

    Major update to courses page 3 column layout Added links to course that had one Designed card containers

--- courses.js
+++ courses.js
@@ -25,132 +25,154 @@ const codecademy = {
         finishDate: "05-28-2022",
         hours: 25,
         weeks: null,
+        image: '/assets/swift-logo.png'
     },
     html: {
         course: "Learn HTML",
         finishDate: "06-12-2022",
         hours: 9,
-        weeks: null
+        weeks: null,
+        image: '/assets/html-logo.png'
     },
     css: {
         course: "Learn CSS",
         finishDate: "06-18-2022",
         hours: 10,
-        weeks: null
+        weeks: null,
+        image: '/assets/css-logo.png'
     },
     cplusplus: {
         course: "Learn C++",
         finishDate: "07-01-2022",
         hours: 25,
-        weeks: null
+        weeks: null,
+        image: '/assets/cplusplus-logo.png'
     },
     intermcplusplus: {
         course: "C++ for Programmers",
         finishDate: "07-05-2022",
         hours: 9,
-        weeks: null
+        weeks: null,
+        image: '/assets/cplusplus-logo.png'
     },
     java: {
         course: "Learn Java",
         finishDate: "07-13-2022",
         hours: 25,
-        weeks: null
+        weeks: null,
+        image: '/assets/java-logo.png'
     },
     beautifulsoup: {
         course: "Learn Web Scraping with BeautifulSoup",
         finishDate: "07-15-2022",
         hours: 1,
-        weeks: null
+        weeks: null,
+        image: '/assets/beautiful-soup.png'
     },
     intermswift: {
         course: "Learn Intermediate Swift",
         finishDate: "07-17-2022",
         hours: 9,
-        weeks: null
+        weeks: null,
+        image: '/assets/swift-logo.png'
     },
     cmdline: {
         course: "Learn Command Line",
         finishDate: "07-23-2022",
         hours: 8,
-        weeks: null
+        weeks: null,
+        image: '/assets/cmd-line.png'
     },
     javascript: {
         course: "Learn JavaScript",
         finishDate: "08-14-2022",
         hours: 20,
-        weeks: null
+        weeks: null,
+        image: '/assets/js-logo.png'
     },
     kotlin: {
         course: "Learn Kotlin",
         finishDate: "10-23-2022",
         hours: 15,
-        weeks: null
+        weeks: null,
+        image: '/assets/kotlin-logo.png'
     },
     nflstats: {
         course: "Analyze NFL Stats with Python Case Study",
         finishDate: "10-26-2022",
         hours: 1,
-        weeks: null
+        weeks: null,
+        image: '/assets/python-logo.png'
     },
     interactivejs: {
         course: "Building Interactive JavaScript Websites",
         finishDate: "11-05-2022",
         hours: 7,
-        weeks: null
+        weeks: null,
+        image: '/assets/js-logo.png'
     },
     ioslevelone: {
         course: "Build iOS Apps with SwiftUI",
         finishDate: "12-28-2022",
         hours: null,
-        weeks: 10
+        weeks: 10,
+        image: '/assets/swift-logo.png'
     },
     iosleveltwo: {
-        course: "Build an Intermediate iOS App with Swift UI",
+        course: "Build an Intermediate iOS App with SwiftUI",
         finishDate: "01-14-2023",
         hours: null,
-        weeks: 14
+        weeks: 14,
+        image: '/assets/swift-logo.png'
     },
     asyncjs: {
         course: "Learn JavaScript: Asynchronous Programming",
         finishDate: "01-14-2023",
         hours: 1,
-        weeks: null
+        weeks: null,
+        image: '/assets/js-logo.png'
     },
     linalg: {
         course: "Linear Algebra",
         finishDate: "04-03-2023",
         hours: 2,
-        weeks: null
+        weeks: null,
+        image: '/assets/linalg-logo.png'
     },
     chatgpt: {
         course: "Intro to ChatGPT",
         finishDate: "04-19-2023",
         hours: 7,
-        weeks: null
+        weeks: null,
+        image: '/assets/chatgpt-icon.png'
     },
     jquery: {
         course: "Learn jQuery",
         finishDate: "05-05-2023",
         hours: 9,
-        weeks: null
+        weeks: null,
+        image: '/assets/jquery-logo.png'
     },
     nodejs: {
         course: "Learn Node.js",
         finishDate: "05-14-2023",
         hours: 7,
-        weeks: null
+        weeks: null,
+        image: '/assets/nodejs-logo.png'
     },
     react: {
         course: "Learn React",
         finishDate: "05-30-2023",
         hours: 15,
-        weeks: null
+        weeks: null,
+        image: '/assets/react-logo.png'
     },
     reactnative: {
         course: "Learn React Native",
         finishDate: "06-02-2023",
         hours: 7,
-        weeks: null
+        weeks: null,
+        image: '/assets/react-logo.png'
     }
 }
 
