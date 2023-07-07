34bd5c081f67967ee7134554e7b05a9d8480f757
Immanuel <immanuel.peter1@gmail.com>
2023-07-06 21:31:18 -0500

    Fixed chatbot; still need to resolve env

--- chat.js
+++ chat.js
@@ -1,11 +1,12 @@
-async function getInitialContext() {  
-    const biography = await fetch('biography.txt');
-    const initialContext = await biography.text();
-    return initialContext
+import { getCompletionFromMessages } from "./responses.js";
+
+async function getInitialContext() {
+  const biography = await fetch("biography.txt");
+  const initialContext = await biography.text();
+  return initialContext;
 }
 
 const initialContext = await getInitialContext();
-console.log(initialContext);
 
 // Collapsible
 var coll = document.getElementsByClassName("collapsible");
@@ -13,137 +14,145 @@ var coll = document.getElementsByClassName("collapsible");
 // var image = document.querySelector(".intro-image");
 
 for (let i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function () {
-        this.classList.toggle("active");
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active");
 
-        var content = this.nextElementSibling;
+    var content = this.nextElementSibling;
 
-        // text.classList.add("move-left")
-        // image.classList.add("move-left-harder");
+    // text.classList.add("move-left")
+    // image.classList.add("move-left-harder");
 
-        if (content.style.maxHeight) {
-            content.style.maxHeight = null;
-            // text.classList.remove("move-left")
-            // image.classList.remove("move-left-harder");
-        } else {
-            content.style.maxHeight = content.scrollHeight + "px";
-        }
-    });
-};
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+      // text.classList.remove("move-left")
+      // image.classList.remove("move-left-harder");
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
 
 function getTime() {
-    let today = new Date();
-    let hours = today.getHours();
-    let minutes = today.getMinutes();
-    let ampm = hours >= 12 ? 'PM' : 'AM';
+  let today = new Date();
+  let hours = today.getHours();
+  let minutes = today.getMinutes();
+  let ampm = hours >= 12 ? "PM" : "AM";
 
-    hours = hours % 12;
-    hours = hours ? hours : 12;
-    minutes = minutes < 10 ? '0' + minutes : minutes;
+  hours = hours % 12;
+  hours = hours ? hours : 12;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
 
-    let time = hours + ':' + minutes + ' ' + ampm;
-    return time;
+  let time = hours + ":" + minutes + " " + ampm;
+  return time;
 }
 
 function firstBotMessage() {
-    const firstMessage = "Hey! I'm Immanuel's chatbot, <b>ImmanuelAI</b>. Ask me anything about Immanuel, and I'll provide you with the scoop on his background, interests, or experiences. Let's start chatting!";
+  const firstMessage =
+    "Hey! I'm Immanuel's chatbot, <b>ImmanuelAI</b>. Ask me anything about Immanuel, and I'll provide you with the scoop on his background, interests, or experiences. Let's start chatting!";
 
-    document.getElementById("botStarterMessage").innerHTML = '<span>' + firstMessage + '</span>';
+  document.getElementById("botStarterMessage").innerHTML =
+    "<span>" + firstMessage + "</span>";
 
-    let time = getTime();
+  let time = getTime();
 
-    document.getElementById("chat-timestap").innerHTML = time;
-    document.getElementById("userInput").scrollIntoView(false);
+  document.getElementById("chat-timestap").innerHTML = time;
+  document.getElementById("userInput").scrollIntoView(false);
 }
 
 firstBotMessage();
 
 let messages = [
-    {
-        role: 'system',
-        content: initialContext
-    },
-    {
-        role: 'assistant',
-        content: "Hey! I'm Immanuel's chatbot, ImmanuelAI. Ask me anything about Immanuel, and I'll provide you with the scoop on his background, interests, or experiences. Let's start chatting!"
-    }
-]
+  {
+    role: "system",
+    content: initialContext,
+  },
+  {
+    role: "assistant",
+    content:
+      "Hey! I'm Immanuel's chatbot, ImmanuelAI. Ask me anything about Immanuel, and I'll provide you with the scoop on his background, interests, or experiences. Let's start chatting!",
+  },
+];
 
 async function getHardResponse(userText) {
-    let botHtml = "";
+  let botHtml = "";
 
-    let botResponse = await getBotResponse(userText);
-    // console.log(botResponse)
-    botHtml = '<p class="botText"><span>' + botResponse + '</span></p>'
-    $('#chatbox').append(botHtml);
+  let botResponse = await getBotResponse(userText);
+  botHtml = '<p class="botText"><span>' + botResponse + "</span></p>";
+  $("#chatbox").append(botHtml);
 
-    messages.push({ role: 'user', content: userText }, { role: 'assistant', content: botResponse });
-    // console.log(messages);
+  messages.push(
+    { role: "user", content: userText },
+    { role: "assistant", content: botResponse }
+  );
+  console.log(messages);
 
-    document.getElementById("chat-bar-bottom").scrollIntoView(true);
+  document.getElementById("chat-bar-bottom").scrollIntoView(true);
 }
 
 async function getBotResponse(input) {
-    try {
-        return await getCompletionFromMessages([...messages, { role: 'user', content: input }])
-    } catch (error) {
-        console.log(error)
-    }
-
+  try {
+    return await getCompletionFromMessages([
+      ...messages,
+      { role: "user", content: input },
+    ]);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 function getResponse() {
-    let userText = $('#textInput').val();
+  let userText = $("#textInput").val();
 
-    if (userText == "") {
-        userText = "What is your name?";
-    }
-    let userHtml = '<p class="userText"><span>' + userText + '</span></p>';
-    document.getElementById("textInput").value = "";
-    $("#chatbox").append(userHtml);
-    document.getElementById("chat-bar-bottom").scrollIntoView(true);
+  if (userText == "") {
+    userText = "What is your name?";
+  }
+  let userHtml = '<p class="userText"><span>' + userText + "</span></p>";
+  document.getElementById("textInput").value = "";
+  $("#chatbox").append(userHtml);
+  document.getElementById("chat-bar-bottom").scrollIntoView(true);
 
-    setTimeout(async () => {
-        await getHardResponse(userText);
-    }, 1000);
+  setTimeout(async () => {
+    await getHardResponse(userText);
+  }, 1000);
 }
 
 function buttonSendText(sampleText) {
-    let userHtml = '<p class="userText"><span>' + sampleText + '</span></p>';
-    document.getElementById("textInput").value = "";
-    $("#chatbox").append(userHtml);
-    document.getElementById("chat-bar-bottom").scrollIntoView(true);
+  let userHtml = '<p class="userText"><span>' + sampleText + "</span></p>";
+  document.getElementById("textInput").value = "";
+  $("#chatbox").append(userHtml);
+  document.getElementById("chat-bar-bottom").scrollIntoView(true);
 }
 
 document.getElementById("send-icon").addEventListener("click", getResponse);
 
 document.getElementById("reset-icon").addEventListener("click", () => {
-    const chatbox = document.getElementById("chatbox");
-    const userTextElements = chatbox.getElementsByClassName("userText");
-    const botTextElements = chatbox.getElementsByClassName("botText");
+  const chatbox = document.getElementById("chatbox");
+  const userTextElements = chatbox.getElementsByClassName("userText");
+  const botTextElements = chatbox.getElementsByClassName("botText");
 
-    // Remove userText elements
-    while (userTextElements.length > 0) {
-        userTextElements[0].remove();
+  // Remove userText elements
+  while (userTextElements.length > 0) {
+    userTextElements[0].remove();
+  }
+
+  // Remove botText elements
+  for (let i = 0; i < botTextElements.length; i++) {
+    const botTextElement = botTextElements[i];
+    if (botTextElement.id !== "botStarterMessage") {
+      const childBotTextElements =
+        botTextElement.getElementsByClassName("botText");
+      if (childBotTextElements.length === 0) {
+        botTextElement.remove();
+      }
     }
+  }
 
-    // Remove botText elements
-    for (let i = 0; i < botTextElements.length; i++) {
-        const botTextElement = botTextElements[i];
-        if (botTextElement.id !== "botStarterMessage") {
-            const childBotTextElements = botTextElement.getElementsByClassName("botText");
-            if (childBotTextElements.length === 0) {
-                botTextElement.remove();
-            }
-        }
-    }
-
-    const newTime = getTime();
-    document.getElementById("chat-timestap").innerHTML = newTime;
-})
+  const newTime = getTime();
+  document.getElementById("chat-timestap").innerHTML = newTime;
+});
 
 document.getElementById("textInput").addEventListener("keydown", (e) => {
-    if (e.code == "Enter") {
-        getResponse();
-    }
+  if (e.code == "Enter") {
+    getResponse();
+  }
 });
--- responses.js
+++ responses.js
@@ -1,37 +1,31 @@
 require('dotenv').config();
 const api_key = process.env.OPENAI_API_KEY;
 
-// import { Configuration, OpenAIApi } from 'openai';
-
-// const config = new Configuration({
-//     apiKey: api_key
-// });
-
-// const openai = new OpenAIApi(config);
-
-
-async function getCompletionFromMessages( messages, model = 'gpt-3.5-turbo', temperature = 0 ) {
-    console.log('clicked');
-    const options = {
-        method: 'POST',
-        headers: {
-            'Authorization': `Bearer ${api_key}`,
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            model: model,
-            messages: messages,
-            maxTokens: 200,
-            temperature: temperature
-        })
-    }
-    try {
-        const completion = await fetch("https://api.openai.com/v1/chat/completions", options)
-        const data = await completion.json();
-        console.log(data);
-        return data.choices[0].message.content.text;
-    } catch(error) {
-        console.log(error);
-        console.log("Something went wrong.")
-    }
-};
+export async function getCompletionFromMessages(
+  messages,
+  model = "gpt-4",
+  temperature = 0
+) {
+  const options = {
+    method: "POST",
+    headers: {
+      "Authorization": `Bearer ${api_key}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      model: model,
+      messages: messages,
+      temperature: temperature,
+    }),
+  };
+  try {
+    const completion = await fetch(
+      "https://api.openai.com/v1/chat/completions",
+      options
+    );
+    const data = await completion.json();
+    return data.choices[0].message.content;
+  } catch (error) {
+    console.log(error);
+  }
+}
